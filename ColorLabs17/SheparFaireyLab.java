68147355c770cda3fd3ed15dfca0ba1a3cb17169
cjshen888 <cjshen88@gmail.com>
2025-11-13 11:49:51 -0600

    updated shepard fairey try 3

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -37,11 +37,11 @@ public class SheparFaireyLab
             spot.setBlue(avg);
             spot.setGreen(avg);
             int r = spot.getRed();//doesn't matter which one you get
-            if (r >= 0 && r <= 63){
+            if (r <= 63){
                 spot.setColor(darkblue);
-            } else if (r >= 64 && r <= 127){
+            } else if (r <= 128){
                 spot.setColor(red);
-            } else if (r >= 128 && r <= 192){
+            } else if (r <= 192){
                 spot.setColor(lightblue);
             } else {
                 spot.setColor(offwhite);
@@ -81,12 +81,34 @@ public class SheparFaireyLab
          /**
           * custom color palette
           */
+         //color palettte #1
          Color pink = new Color(255,77,164);
          Color royalblue = new Color(97,137,255);
          Color orange = new Color(255,199,77);
-         Color lightgreen = new Color(77,255,169);
-         Color green = new Color(158,255,0);
+         Color darkgreen = new Color(0,255,127);
+         Color lightgreen = new Color(158,255,0);
          
+         //color palette #2
+         Color r1 = new Color(255,99,71);
+         Color r2 = new Color(178,34,34);
+         Color r3 = new Color(255,160,122);
+         Color r4 = new Color(255,215,0);
+         Color r5 = new Color(255,192,203);
+         
+         //color palette #3
+         Color b1 = new Color(0,128,128);
+         Color b2 = new Color(32,178,170);
+         Color b3 = new Color(64,224,208);//(102,205,170);
+         Color b4 = new Color(175,238,238);
+         Color b5 = new Color(224,255,255);
+         
+         //color palette #4
+         Color s1 = new Color(25,25,112);//midnight
+         Color s2 = new Color(65,105,225);//royal blue
+         Color s3 = new Color(100,149,237);//cornflower blue
+         Color s4 = new Color(255,160,122);//salmon
+         Color s5 = new Color(250,128,114); //light salmon
+         Color s6 = new Color(255,228,181); //moccasin
          Pixel[] pixels2;
          pixels2= me2.getPixels();
          for (Pixel spot : pixels2) {
@@ -95,30 +117,62 @@ public class SheparFaireyLab
             spot.setBlue(avg);
             spot.setGreen(avg);
             int r = spot.getRed();
-            /* //Try 1---------------------------------------------------
-            if (r >= 0 && r <= 63){
+             //Try 1---------------------------------------------------
+            /*if (r <= 63){
                 spot.setColor(royalblue);
-            } else if (r >= 64 && r <= 127){
+            } else if (r <= 128){
                 spot.setColor(pink);
-            } else if (r >= 128 && r <= 192){
+            } else if (r <= 192){
                 spot.setColor(orange);
             } else {
                 spot.setColor(green);
-            } */
-            
-            // Try 2 ----------------------------------------------------
-            if (r >= 0 && r <= 70){
+            } 
+            */
+            // Try 2: added fifth color  ----------------------------------------------------
+            /*if (r <= 63){
                 spot.setColor(royalblue);
-            } else if (r >= 70 && r <= 140){
+            } else if (r <= 128){
                 spot.setColor(pink);
-            } else if (r >= 140 && r <= 192){
+            } else if (r <= 192){
                 spot.setColor(orange);
+            } else if (r<= 220){
+                spot.setColor(lightgreen);
             } else {
-                spot.setColor(green);
+                spot.setColor(darkgreen);
+            }*/
+            //Try 3: different shades of same color --------------------------------------------------
+            /*if (r <= 63){
+                spot.setColor(b1);
+            } else if (r <= 128){
+                spot.setColor(b2);
+            } else if (r <= 192){
+                spot.setColor(b3);
+            } else if (r<= 220){
+                spot.setColor(b4);
+            } else if (r<=240){
+                spot.setColor(s5);*/
+            //Try 4: 
+            if (r <= 63){
+                //spot.setColor(firebrick);
+                spot.setColor(s1);
+            } else if (r <= 130){
+                //spot.setColor(tomato);
+                spot.setColor(s2);
+            } else if (r <= 200){
+                //spot.setColor(lightsalmon);
+                spot.setColor(s3);
+            } else if (r<= 210){
+                //spot.setColor(gold);
+                spot.setColor(s4);
+            } else if (r<=220){
+                //spot.setColor(lightpink);
+                spot.setColor(s5);
+            } else {
+                spot.setColor(s6);
             }
-         }
+        }
          me2.explore();
-         me2.write("images/SFTry1.jpg");
+         me2.write("images/SFTry3.jpg");
          //this writes a copy of the pic!!!! RENAME PIC!!!!
          //me1.write("images/SFTry1.jpg");
     }//main       
